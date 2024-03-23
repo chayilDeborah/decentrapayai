e79c0be63bf79d199046a0d8ee94c4fed98878df
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-03-23 13:14:57 +0100

    feat: features and tokenomics

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -8,7 +8,7 @@ const Features = () => {
         <section className='text-center text-white w-full '>
         <h1 className='lg:text-[40px] text-[28px] lg:leading-[48.4px] '><span className='leverage'> Our</span> Features</h1>
         <p className='font-medium lg:text-[18px] text-[15px] leading-[24px] lg:leading-[21.78px] tracking-[0.2px] mt-[14px] lg:mt-[20px] mb-[30px] lg:mb-[50px] '>Lorem ipsum dolor sit amet consectetur. Nec id turpis arcu quam etiam ut.</p>
-        <section className='grid grid-cols-1 lg:grid-cols-3 '>
+        <section className='grid grid-cols-1 lg:grid-cols-3 flex justify-center lg:w-full w-[90%] mx-auto lg:mx-[0px] '>
             <div className='border border-[1px] rounded-[10px] pl-[27px] w-[336px] '>
                 <Image src={feat} alt='feature-img' className='mx-auto my-[40px] ' />
                 <h1 className='font-extrabold text-[25px] leading-[30.2px] tracking-[0.2px] text-left '>Escrow Payments</h1>
--- app/components/Tokenomics.tsx
+++ app/components/Tokenomics.tsx
@@ -2,33 +2,33 @@ import React from 'react'
 
 const Tokenomics = () => {
     return (
-        <main className='flex justify-center lg:mt-[140px] '>
-            <section className='lg:w-[87%] rounded-[10px] grad p-[2.3px]  '>
+        <main className='flex justify-center mt-[70px] lg:mt-[140px] '>
+            <section className='lg:w-[87%] w-[90%] rounded-[10px] grad p-[2.3px]  '>
                 <div className='flex justify-center rounded-[10px] bg-[#0b0b0b] h-full w-full'>
                     <div>
-                        <div className=' lg:w-[185px] h-[64px] lg:rounded-[60px] text-[#FFFFFF] mt-[80px] mx-auto grad p-[1.3px]'>
-                            <div className='flex justify-center lg:rounded-[60px] items-center mx-auto bg-[#0b0b0b]  flex h-full w-full'>
+                        <div className=' w-[185px] h-[64px] rounded-[60px] text-[#FFFFFF] mt-[80px] mx-auto grad p-[1.3px]'>
+                            <div className='flex justify-center rounded-[60px] items-center mx-auto bg-[#0b0b0b]  flex h-full w-full'>
                                 <h2 className='lg:text-[19.4px] lg:leading-[24px] lg:mr-[16px] '>Tokenomics</h2>
 
                             </div>
                         </div>
-                        <h1 className='leverage font-extrabold lg:text-[25px] leading-[30.26px] tracking-[0.2px] lg:mt-[33px] lg:mb-[69px] text-center '>DecentraPay Token</h1>
-                        <div className='flex justify-between lg:text-[16px] lg:w-[860px] text-white font-bold lg:leading-[19.36px] tracking-[0.2px] mb-[80px] ' >
+                        <h1 className='leverage font-extrabold text-[25px] leading-[30.26px] tracking-[0.2px] my-[33px] lg:mb-[69px] text-center '>DecentraPay Token</h1>
+                        <div className='lg:flex justify-between text-[18px] lg:text-left text-center lg:text-[16px] lg:w-[860px] text-white font-bold lg:leading-[19.36px] tracking-[0.2px] mb-[80px] ' >
                             <div>
                                 <h1 className=' mb-[10px]'>Total Supply:</h1>
                                 <h1>10,000,000</h1>
                             </div>
-                            <div className='grad w-[1px] h-[67px] '></div>
+                            <div className='grad lg:w-[1px] h-[1.3px] lg:h-[67px] my-[25px] lg:my-[0px] '></div>
                             <div>
                                 <h1 className=' mb-[10px]'>Ticker:</h1>
                                 <h1>DPAY</h1>
                             </div>
-                            <div className='grad w-[1px] h-[67px] '></div>
+                            <div className='grad lg:w-[1px] h-[1.3px] lg:h-[67px]  my-[25px] lg:my-[0px]  '></div>
                             <div>
                                 <h1 className=' mb-[10px]'>Taxes:</h1>
                                 <h1>5/5</h1>
                             </div>
-                            <div className='grad w-[1px] h-[67px] '></div>
+                            <div className='grad lg:w-[1px] h-[1.3px] lg:h-[67px]  my-[25px] lg:my-[0px]  '></div>
                             <div>
                                 <h1 className=' mb-[10px]'>Token Name:</h1>
                                 <h1>DecentraPay AI</h1>
